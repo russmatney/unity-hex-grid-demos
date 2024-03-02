51cc2de3526d9b04b33f7ede5d07c3127a0f086b
Russell Matney <russell.matney@gmail.com>
2024-03-02 18:04:07 -0500

    feat: rough fog of war effect working

--- Assets/MapInputManager.cs
+++ Assets/MapInputManager.cs
@@ -8,20 +8,43 @@ public class MapInputManager : MonoBehaviour
 {
 
     [SerializeField] public Tilemap baseTiles;
+    [SerializeField] public Tilemap fogOfWar;
     [SerializeField] public GameObject cursor;
+
+    public int vision = 1;
 
     // Update is called once per frame
     void Update()
     {
         UpdateCursorPosition();
+        if (Input.GetMouseButtonDown(0)) {
+            UpdateFogOfWar();
+        }
     }
 
-    void UpdateCursorPosition() {
+    void UpdateCursorPosition()
+    {
         Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3Int coordinate = baseTiles.WorldToCell(mouseWorldPos);
         Vector3 worldCoordinate = baseTiles.CellToWorld(coordinate);
         worldCoordinate.z = 0;
-        Debug.Log("hovering tile " + coordinate.ToString() + " " + worldCoordinate.ToString());
         cursor.transform.position = worldCoordinate;
+    }
+
+    void UpdateFogOfWar()
+    {
+        Vector3Int cursorTileCoord = fogOfWar.WorldToCell(cursor.transform.position);
+
+        for (int x = -vision; x <= vision; x++)
+        {
+            for (int y = -vision; y <= vision; y++)
+            {
+                Vector3Int coord = cursorTileCoord + new Vector3Int(x, y, 0);
+                // clear tiles!
+                fogOfWar.SetTile(coord, null);
+                Debug.Log("cleared tile " + coord.ToString());
+            }
+        }
+
     }
 }
